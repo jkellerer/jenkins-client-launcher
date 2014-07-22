69aaa1d067b4f236ef2ec348397883e7f7e9f618
Juergen Kellerer <juergen_kellerer@trendmicro.de>
2014-07-22 17:22:40 +0200

    Code: Added missing license headers.

--- launcher/util/hostname_windows.go
+++ launcher/util/hostname_windows.go
@@ -1,3 +1,6 @@
+// Copyright 2014 The jenkins-client-launcher Authors. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found in the LICENSE file.
+
 package util
 
 import (
